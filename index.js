c083358e9066b8125313bd23c1869dda5d81cafe
Inga Zhelnova <detal.07@mail.ru>
2024-02-21 21:46:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+{
+  const item = "Assol";
+  const count = 100;
+  const category = "A home dress";
+  const price = 15;
+
+	console.log(item);
+  console.log(`Total price is: ${count * price}`);
+}
+
+
+
+
+
+
+
+
